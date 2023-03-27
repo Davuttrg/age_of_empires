91c1dd771816fccfeee2dffb38e5e36e2d67ee76
Davuttrg <davut.0773@gmail.com>
2023-03-28 01:03:31 +0300

    feat: add style to ages filter

--- src/app/components/ui/units/age-filter/age-filter.component.ts
+++ src/app/components/ui/units/age-filter/age-filter.component.ts
@@ -9,6 +9,28 @@ export class AgeFilterComponent implements OnInit {
 
   constructor() { }
 
+  ages: { name: string, value: string }[] = [{
+    name: "All",
+    value: "all"
+  },
+  {
+    name: "Dark",
+    value: "dark"
+  },
+  {
+    name: "Feudal",
+    value: "feudal"
+  },
+  {
+    name: "Castle",
+    value: "castle"
+  },
+  {
+    name: "Imperial",
+    value: "imperial"
+  }]
+  selectedAge: string = "all"
+
   ngOnInit(): void {
   }
 
