9ae52d8a3e4b96187d522bd6dc0d3b295897b887
Davuttrg <davut.0773@gmail.com>
2023-03-27 22:56:45 +0300

    feat: create pages & set routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UnitComponent } from './pages/units/unit/unit.component';
+import { UnitsComponent } from './pages/units/units.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "units",
+    children: [
+      {
+        path: '',
+        component: UnitsComponent
+      },
+      {
+        path: '/:id',
+        component: UnitComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UnitComponent } from './pages/units/unit/unit.component';
+import { UnitsComponent } from './pages/units/units.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UnitsComponent,
+    UnitComponent
   ],
   imports: [
     BrowserModule,
